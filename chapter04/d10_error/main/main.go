6ef84b61323202529c30f071de71b7fed544d328
cstsncv <cstsncv@sina.com>
2020-02-13 15:19:42 +0800

    test

--- chapter04/d10_error/main/main.go
+++ chapter04/d10_error/main/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 // Go不支持传统的Try .. catch..finally
 // Go中引入的处理方式为: defer,panic,recover
@@ -20,8 +23,27 @@ func test() {
 	fmt.Println("res = ", res)
 }
 
+//	自定义错误:  函数读取传入信息,无法匹配则返回一个自定义错误
+func readConfig(name string) (err error) {
+	if name == "config.ini" {
+		return nil
+	}
+	//返回一个自定义错误
+	return errors.New("输入信息错误")
+}
+
+func test2() {
+	err := readConfig("asd")
+	if err != nil {
+		//如果有错误,输出该错误并终止程序
+		panic(err)
+	}
+	fmt.Println("test2()继续执行....")
+}
+
 func main() {
 
 	test()
+	test2()
 	fmt.Println("over")
 }
